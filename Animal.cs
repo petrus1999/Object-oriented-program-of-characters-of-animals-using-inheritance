c80234cbe22092a71e5f653aa3a9fca23b869d8d
petrus <pshai671@gmail.com>
2023-01-25 14:30:41 +0200

    Initial commit

---
+++ Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_oriented_program_that_print_out_characters_of_animals
+{
+   class Animal
+    {
+        public string Colour;
+        public int Weight;
+        public string Animaltype;
+    }
+    class Dog : Animal
+    {
+        public Dog(string colour, int weight, string animaltype)
+        {
+            Colour = colour;
+            Weight = weight;
+            Animaltype = animaltype;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Object_oriented_program_that_print_out_characters_of_animals;
+
+class Program
+{
+    static void Main(string[] args) {
+        Dog dog1 = new Dog("BLACK", 10KG, "PITBULL");
+        Dog dog2 = new Dog("WHITE", 30KG, "GERMAN SHERPERD");
+        Dog dog3 = new Dog("BROWN", 18KG, "PITBULL");
+        Dog dog4 = new Dog("BLACK&WHITE", 25KG, "POODLE");
+        Dog dog5 = new Dog("BROWNISH", 27KG, "CHIHUAHUA");
+        Dog dog 6 = new Dog("BLACK", 40, "BULLDOG");
+
+        Console.WriteLine(dog1.Colour + " " + dog1.Weight + " " + dog1.Animaltype);
+        Console.WriteLine(dog2.Colour + " " + dog2.Weight + " " + dog2.Animaltype);
+        Console.WriteLine(dog3.Colour + " " + dog3.Weight + " " + dog3.Animaltype);
+        Console.WriteLine(dog4.Colour + " " + dog4.Weight + " " + dog4.Animaltype);
+        Console.WriteLine(dog5.Colour + " " + dog5.Weight + " " + dog5.Animaltype);
+        Console.WriteLine(dog6.Colour + " " + dog6.Weight + " " + dog6.Animaltype);
+
+    }
+    
+    
+}
